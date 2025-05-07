48467b4f481c0553c231a4efce414a6b669084de
StefanoPellegrino1 <spellegrino@frba.utn.edu.ar>
2025-05-07 13:44:38 -0300

    prod url

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"http://localhost:3003/api",
+  baseURL:"https://fratelli-back.vercel.app/api",
   headers: {
     "Content-Type": "application/json"
   }
